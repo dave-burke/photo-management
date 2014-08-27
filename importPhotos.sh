634996a2454a8843666ecb2eec3b67de6f8205ed
Dave Burke <dburke84@gmail.com>
2014-08-26 21:27:30 -0500

    Lots of clean up
    More system checking on init
    Added functions for common tasks
    Improved argument parsing
    Added draft code for filtering photos with feh

--- importPhotos.sh
+++ importPhotos.sh
@@ -1,31 +1,150 @@
 #!/bin/bash
 
-tmpPhotoDir=/tmp/photos
-targetPhotoDir=~/photos
-if [ $# -eq 0 ]; then
-	echo You must specify either "dave" "suz" or "cam"
-	exit 1
-elif [ $1 == "dave" -o $1 == "-d" ]; then
-	echo "Backing up Dave's cell"
-	device=mtp
-	mountPoint=/media/android
-	srcPhotoDir=${mountPoint}/Card/DCIM
-elif [ $1 == "suz" -o $1 == "-s" ]; then
-	echo "Backing up Suz's cell"
-	device=mtp
-	mountPoint=/media/android
-	srcPhotoDir=${mountPoint}/DCIM
-elif [ $1 == "cam" -o $1 == "-c" ]; then
-	echo "Backing up camera"
-	device=/dev/disk/by-label/CAMERA
-	mountPoint=/media/cam
-	srcPhotoDir=${mountPoint}/DCIM
+# TODO test safe_delete, split into multiple scripts:
+# imports.sh (common functions)
+# import-photos.sh (move photos from device to tmp dir)
+# sort-photos.sh (use feh to pick photos, use exiftool to sort into dated directories)
+# backup-photos.sh (to s3)
+#********************FUNCTION DECLARATIONS********************
+verify_command() {
+	VERIFIED_COMMAND=""
+
+	SEARCH_DESCRIPTION=$1
+	if [[ $# -gt 1 ]]; then
+		shift
+	fi
+
+	echo Searching for ${SEARCH_DESCRIPTION}
+	for f in $@; do
+		if command -v $f >/dev/null; then
+			echo Found $(command -v $f)
+			VERIFIED_COMMAND=${f}
+			return 0
+		else
+			echo "No ${f}..."
+			shift
+		fi
+	done
+	echo "No ${SEARCH_DESCRIPTION} found!"
+	return 1
+}
+
+safe_delete() {
+	if command -v kioclient >/dev/null; then
+		echo "Trashing files"
+		kioclient move "${1}" trash:/
+	else
+		echo Okay to delete ${1}?
+		read isOk
+		if [ $isOk == "yes" ]; then
+			rm -rfv "${1}"
+		else
+			echo "Did not delete ${1}"
+		fi
+	fi
+}
+
+is_mtp() {
+	if [[ ${1} == "mtp" ]]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+die() {
+	echo $1
+	if [[ -z $2 ]]; then
+		exit 1
+	else
+		exit $2
+	fi
+}
+
+#********************PARSE CLI********************
+while [ "$1" ]; do
+	case $1 in
+		-d|--device)
+			device=${2}
+			shift
+			;;
+		-s|--src-dir)
+			srcPhotoDir=$2
+			shift
+			;;
+		-t|--target_dir)
+			targetPhotoDir=$2
+			shift
+			if [[ -d ${targetPhotoDir} ]]; then
+				echo Target photo dir is ${targetPhotoDir}
+			else
+				die "${targetPhotoDir} is not a valid target photo directory" 1
+			fi
+			;;
+		-p|--preset)
+			preset=$2
+			shift
+			if [ ${preset} == "android-sd" ]; then
+				echo "Backing up from Android SD card"
+				device=mtp
+				mountPoint=/media/android
+				srcPhotoDir=${mountPoint}/Card/DCIM
+			elif [ ${preset} == "android" ]; then
+				echo "Backing up from Android internal storage"
+				device=mtp
+				mountPoint=/media/android
+				srcPhotoDir=${mountPoint}/DCIM
+			elif [ ${preset} == "cam" ]; then
+				echo "Backing up camera"
+				device=/dev/disk/by-label/CAMERA
+				mountPoint=/media/cam
+				srcPhotoDir=${mountPoint}/DCIM
+			fi
+			;;
+		-*)
+			die "unrecognized option: $1"
+			;;
+		*)
+			break
+			;;
+	esac
+	shift
+done
+
+#********************SYSTEM CHECK********************
+
+verify_command exiftool || exit 1
+verify_command feh || exit 1
+
+# Verify device
+if is_mtp ${device} ; then
+	verify_command "MTP tool" simple-mtpfs || exit 1
+	MTP_UTIL=${VERIFIED_COMMAND}
+elif [[ ! -b ${device} ]]; then
+	die "Could not find device \"${device}\"!"
 fi
 
-#********************MOUNT THE DEVICE********************
+# Default target dir if none specified
+[[ ! -z ${targetPhotoDir} ]] || targetPhotoDir=~/photos
+
+[[ -d ${targetPhotoDir} ]] || die "No target directory at ${targetPhotoDir}"
+
+# Set tmp photo dir
+tmpPhotoDir=/tmp/photos
+
+echo "***SUMMARY***"
+echo Device: ${device}
+echo Mount point: ${mountPoint}
+echo Source dir: ${srcPhotoDir}
+echo Temp dir: ${tmpPhotoDir}
+echo Target dir: ${targetPhotoDir}
+echo "*************"
+
+exit 0
+
 if [ ! -d "${srcPhotoDir}" ]; then #only mount if it's not already done
-	if [ ${device} == "mtp" ]; then
-		simple-mtpfs ${mountPoint}
+	if is_mtp ${device} ; then
+		${MTP_UTIL} ${mountPoint}
 	else
 		if [ ! -b ${device} ]; then
 			echo "Can't find a device at ${device}"
@@ -37,16 +156,14 @@ fi
 
 #********************VERIFY THE PHOTO SOURCE DIRECTORY********************
 if [ ! -d "${srcPhotoDir}" ]; then
-	echo "Couldn't find source photo directory on device: ${srcPhotoDir}"
-	exit 1
+	die "Couldn't find source photo directory on device: ${srcPhotoDir}"
 fi
 
-#********************CREATE PHOTO DIRECTORY********************
+#********************CREATE TEMP PHOTO DIRECTORY********************
 if [ ! -d "${tmpPhotoDir}" ]; then
 	mkdir -v "${tmpPhotoDir}"
 	if [ ! -d "${tmpPhotoDir}" ]; then
-		echo "Couldn't make directory at ${tmpPhotoDir}"
-		exit 1
+		die "Couldn't make directory at ${tmpPhotoDir}"
 	fi
 else
 	echo ${tmpPhotoDir} already exists.
@@ -57,48 +174,34 @@ echo Copying photos...
 rsync -av -h --progress --exclude="cache" "${srcPhotoDir}/" "${tmpPhotoDir}"
 if [ $? -eq 0 ]; then
 	echo "Success!"
-	if hash kioclient >/dev/null; then
-		echo "Trashing files"
-		kioclient move "${srcPhotoDir}" trash:/
-	else
-		echo Okay to delete source?
-		read isOk
-		if [ $isOk == "yes" ]; then
-			rm -rfv "${srcPhotoDir}"
-		else
-			echo "Did not delete source directory ${srcPhotoDir}"
-		fi
-	fi
+	safe_delete ${srcPhotoDir}
 else
-	echo "Failed to sync photos!"
-	exit 1
+	die "Failed to sync photos!"
 fi
 
+#********************Filter photos*************************
+echo Filter your photos...
+echo "Rotate with <>"
+echo "Delete with d"
+feh_file=kept.txt
+feh -d -g 800x600 -f ${feh_file} ${tmpPhotoDir}
+
 #********************Organize photos*********************
-echo Sorting photos...
-exiftool -ext '*' --ext CTG '-Directory<CreateDate' -d ${targetPhotoDir}/%Y-%m -r ${tmpPhotoDir}
-if [ $? -eq 0 ]; then
-	echo "Success!"
-	if hash kioclient >/dev/null; then
-		echo "Trashing files"
-		kioclient move "${tmpPhotoDir}" trash:/
+echo Organizing photos...
+while read f; do
+	exiftool -ext '*' --ext CTG '-Directory<CreateDate' -d ${targetPhotoDir}/%Y-%m ${f}
+	if [ $? -eq 0 ]; then
+		echo "Sorted ${f}!"
+		safe_delete ${f}
 	else
-		echo Okay to delete tmp dir?
-		read isOk
-		if [ $isOk == "yes" ]; then
-			rm -rfv "${tmpPhotoDir}"
-		else
-			echo "Did not delete source directory ${tmpPhotoDir}"
-		fi
+		die "Failed to organize ${f}!"
 	fi
-else
-	echo "Failed to sort photos!"
-	exit 1
-fi
+done < ${feh_file}
+safe_delete ${tmpPhotoDir}
 
 #********************UNMOUNT THE DEVICE********************
 sync ; sleep 2
-if [ ${device} == "mtp" ]; then
+if is_mtp ${device} ; then
 	fusermount -u "${mountPoint}"
 else
 	umount -v "${mountPoint}"
