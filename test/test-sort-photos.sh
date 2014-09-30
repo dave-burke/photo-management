49b5f640331a78e45df49f1af24f52fc04826081
Dave Burke <dburke84@gmail.com>
2014-09-29 21:12:17 -0500

    test: Make sort test non-interactive

--- test/test-sort-photos.sh
+++ test/test-sort-photos.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+FAILED=false
+fail_test() {
+	echo "${1}"
+	FAILED=true
+}
+
 ### INIT
 
 cd $(dirname ${0})
@@ -19,9 +25,17 @@ cp -arv samples/* "test-data/source"
 
 ### VERIFY
 
-if command -v tree >/dev/null; then
-	tree test-data
+[[ -f "test-data/target/2005-03/London_-_Crystal_Palace_-_Victorian_Dinosaurs_1.jpg" ]] || fail_test "London_-_Crystal_Palace_-_Victorian_Dinosaurs_1.jpg was not sorted!"
+[[ -f "test-data/target/2010-04/4521167129_39bc0b7ab6_z.jpg" ]] || fail_test "4521167129_39bc0b7ab6_z.jpg was not sorted!"
+[[ -f "test-data/target/2010-10/saurischia_1.jpg" ]] || fail_test "saurischia_1.jpg was not sorted!"
+[[ -f "test-data/target/2014-09/6446470849_39efc3002a_z.jpg" ]] || fail_test "6446470849_39efc3002a_z.jpg was not sorted!"
+[[ -f "test-data/target/2014-09/t-rex.jpeg" ]] || fail_test "t-rex.jpeg was not sorted!"
+
+if [[ "${FAILED}" == true ]]; then
+	echo "FAILED!"
+	exit 1
 else
-	ls -A test-data/*
+	echo "SUCCESS!"
+	exit 0
 fi
 
