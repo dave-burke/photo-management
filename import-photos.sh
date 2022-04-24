a40277ce0c7334c4d00e080c1a34e3721f169ed2
Dave Burke <dburke84@gmail.com>
2022-04-23 20:19:54 -0500

    Remove code for mounting usb and mtp devices

--- import-photos.sh
+++ import-photos.sh
@@ -5,30 +5,6 @@ set -e
 source "$(dirname $(realpath ${0}))/common.sh"
 
 #********************FUNCTIONS********************
-is_mtp() {
-	if [[ "${1}" == "mtp" ]]; then
-		return 0
-	else
-		return 1
-	fi
-}
-
-is_mounted() {
-	local dev="$(realpath ${1})"
-	if [[ "${dev}" != "${1}" ]]; then
-		echo "Device ${1} is actually ${dev}"
-	fi
-	if [[ ! -b "${dev}" ]]; then
-		echo "${dev} is not a block device!"
-		return 1
-	fi
-	if mount | grep "${dev}" >/dev/null; then
-		return 0
-	else
-		return 1
-	fi
-}
-
 get_safe_filename() {
 	unset SAFE_NAME
 	local dirname=$(dirname "${1}")
@@ -101,21 +77,6 @@ safe_flatten() {
 #********************PARSE CLI********************
 while [ "$1" ]; do
 	case $1 in
-		-d|--device)
-			device="${2}"
-			shift
-			if is_mtp "${device}" ; then
-				verify_command "MTP tool" simple-mtpfs || die "simple-mtpfs tools is required for mtp devices"
-				MTP_UTIL=${VERIFIED_COMMAND}
-			elif [[ ! -b "${device}" ]]; then
-				die "Could not find block device \"${device}\"!"
-			fi
-			;;
-		-m|--mount-point)
-			mount_point="${2}"
-			shift
-			[[ -d "${mount_point}" ]] || die "${mount_point} is not a valid mount point"
-			;;
 		-u|--use-sudo)
 			use_sudo=true
 			;;
@@ -143,35 +104,6 @@ done
 
 #********************VERIFY CLI********************
 [[ -n "${import_source_dir}" ]] || die "[-s|--src-dir] is required"
-if [ -n "${device}" -a -z "${mount_point}" ]; then
-	die "[-m|--mount-point] is required when you specify a [-d|--device]"
-fi
-if [ -n "${mount_point}" -a -z "${device}" ]; then
-	die "[-d|--device] is required when you specify a [-m|--mount-point]"
-fi
-
-#********************MOUNT DEVICE********************
-if [ -n "${device}" ]; then
-	echo "Mounting device..."
-	if is_mtp "${device}" ; then
-		${MTP_UTIL} "${mount_point}"
-		[[ $? -eq 0 ]] || die "MTP mount failed!"
-	else
-		if is_mounted "${device}"; then
-			echo "${device} is already mounted."
-		else
-			if ${use_sudo}; then
-				current_uid=$(id -u)
-				current_gid=$(id -g)
-				sudo mount -v -o umask=0022,uid=${current_uid},gid=${current_gid} "${device}" "${mount_point}"
-			else
-				mount -v "${device}" "${mount_point}"
-			fi
-		fi
-	fi
-else
-	echo "No device to mount."
-fi
 
 #********************VERIFY THE PHOTO SOURCE DIRECTORY********************
 echo -n "Checking source directory..."
@@ -203,21 +135,6 @@ rm -v "${import_target_dir}"/*.CTG || echo "No CTG files"
 if [[ -z "${no_delete_src}" ]]; then
 	echo "Deleting source files from ${import_source_dir}..."
 	safe_delete "${import_source_dir}"
-fi
-
-#********************UNMOUNT THE DEVICE********************
-if [ -n "${device}" ]; then
-	echo "Unmounting the device"
-	sync ; sleep 2
-	if is_mtp "${device}" ; then
-		fusermount -u "${mount_point}"
-	else
-		if ${use_sudo}; then
-			sudo umount -v "${mount_point}"
-		else
-			umount -v "${mount_point}"
-		fi
-	fi
 fi
 
 echo Done!
